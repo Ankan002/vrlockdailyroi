08043b242203ece4a4474504fd582109af851d40
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-02-02 14:22:45 +0530

    feat: :sparkles: update calculate-reward

---
+++ helper/get-daily-reward-level.js
@@ -0,0 +1,59 @@
+const level = {
+  Junior: '$0.5',
+  Pro: 'Domestic Tour',
+  Starter: 'Laptop',
+  'Pro Starter': '$25',
+  Professional: '$100',
+  Expert: '$200',
+  Senate: '$300',
+  Director: '$500',
+  President: '$100'
+};
+
+export const getDailyRewardLevel = (amount) => {
+  if (amount >= 2500)
+    return {
+      level: 'Junior',
+      reward: level.Junior
+    };
+  else if (amount >= 7500)
+    return {
+      level: 'Pro',
+      reward: level.Pro
+    };
+  else if (amount >= 25000)
+    return {
+      level: 'Starter',
+      reward: level.Starter
+    };
+  else if (amount >= 100000)
+    return {
+      level: 'Pro Starter',
+      reward: level['Pro Starter']
+    };
+  else if (amount >= 250000)
+    return {
+      level: 'Professional',
+      reward: level.Professional
+    };
+  else if (amount >= 500000)
+    return {
+      level: 'Expert',
+      reward: level.Expert
+    };
+  else if (amount >= 1000000)
+    return {
+      level: 'Senate',
+      reward: level.Senate
+    };
+  else if (amount >= 1500000)
+    return {
+      level: 'Director',
+      reward: level.Director
+    };
+  else if (amount >= 2500000)
+    return {
+      level: 'President',
+      reward: level.President
+    };
+};
---
+++ pages/api/daily-reward.js
@@ -0,0 +1,96 @@
+import User from '../../helper/Modal/User';
+import CareerReward from '../../helper/Modal/Records/CareerReward';
+import { parse, differenceInHours, subDays, format } from 'date-fns';
+import { calculateRewards } from '../../helper/calculate-reward';
+import { getDailyRewardLevel } from '../../helper/get-daily-reward-level';
+
+const dailyReward = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(400).json({
+      success: false,
+      error: 'Invalid Request Method!!'
+    });
+  }
+
+  const { user_id } = req.body;
+
+  try {
+    const user = await User.findById(user_id);
+    const rewards = await CareerReward.find({
+      user_id: user.id
+    })
+      .sort('-createdAt')
+      .limit(1);
+
+    if (lastReward.length > 0) {
+      const lastRewardTimeString = rewards[0].time_granted;
+      const lastRewardTime = parse(
+        lastRewardTimeString,
+        'dd MM yyyy HH:mm',
+        new Date()
+      );
+
+      const currentDate = new Date();
+
+      const hourDifference = differenceInHours(currentDate, lastRewardTime);
+
+      const totalDaysToBeGranted = hourDifference % 24;
+
+      if (totalDaysToBeGranted <= 0) {
+        return res.status(400).json({
+          success: false,
+          error: 'Reward Already Granted'
+        });
+      }
+
+      const eachDayRewardToBeGranted = await calculateRewards(user.id);
+      const dailyReward = getDailyRewardLevel(eachDayRewardToBeGranted);
+
+      for (let i = 0; i < totalDaysToBeGranted; i++) {
+        const date = subDays(new Date(), i);
+        const dateString = format(date, 'dd MM yyyy HH:mm');
+
+        await CareerReward.create({
+          user_id: user.id,
+          reward_level: dailyReward.level,
+          reward_granted: dailyReward.reward,
+          time_granted: dateString
+        });
+      }
+
+      return res.status(200).json({
+        success: true
+      });
+    }
+
+    const rewardToBeGranted = await calculateRewards(user.id);
+    const dailyReward = getDailyRewardLevel(rewardToBeGranted);
+
+    const dateString = format(new Date(), 'dd MM yyyy HH:mm');
+
+    await CareerReward.create({
+      user_id: user.id,
+      reward_level: dailyReward.level,
+      reward_granted: dailyReward.reward,
+      time_granted: dateString
+    });
+
+    res.status(200).json({
+      success: true
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({
+        success: false,
+        error: error.message
+      });
+    }
+
+    return res.status(500).json({
+      success: false,
+      error: 'Internal Server Error!!'
+    });
+  }
+};
+
+export default dailyReward;
